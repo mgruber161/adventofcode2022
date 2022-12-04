10a468022aa8fb60e7b22991d7661360b2f2b207
Manuel Gruber <grubermk97@gmail.com>
2022-12-04 13:16:05 +0100

    simplified day 4

--- aoc4/Program.cs
+++ aoc4/Program.cs
@@ -7,19 +7,17 @@ namespace aoc4
         static void Main(string[] args)
         {
             var input = File.ReadAllLines(@"C:\Users\grube\Source\repos\AOC\aoc4\aoc4.txt");
-            int lowerBound = 0;
-            int upperBound = 0;
+            int lowerBound, upperBound, count = 0;
             var assignments = new List<int>();
             var groupAssignments = new List<int[]>();
-            int count = 0;
             foreach (var item in input)
             {
                 var pairs = item.Split(',');
                 foreach (var pair in pairs)
                 {
                     var limits = pair.Split('-');
-                    if (Int32.TryParse(limits[0], out lowerBound)) { }
-                    if (Int32.TryParse(limits[1], out upperBound)) { }
+                    Int32.TryParse(limits[0], out lowerBound);
+                    Int32.TryParse(limits[1], out upperBound);
                     for (int i = lowerBound; i <= upperBound; i++)
                         assignments.Add(i);
                     groupAssignments.Add(assignments.ToArray());
