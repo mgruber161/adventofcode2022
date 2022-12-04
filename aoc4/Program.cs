f48b1dc9c56f0a8569a2e0bf838b00ec871ba192
Manuel Gruber <grubermk97@gmail.com>
2022-12-04 10:35:13 +0100

    day 4

---
+++ aoc4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace aoc4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"C:\Users\grube\Source\repos\AOC\aoc4\aoc4.txt");
+            int lowerBound = 0;
+            int upperBound = 0;
+            var assignments = new List<int>();
+            var groupAssignments = new List<int[]>();
+            int count = 0;
+            foreach (var item in input)
+            {
+                var pairs = item.Split(',');
+                foreach (var pair in pairs)
+                {
+                    var limits = pair.Split('-');
+                    if (Int32.TryParse(limits[0], out lowerBound)) { }
+                    if (Int32.TryParse(limits[1], out upperBound)) { }
+                    for (int i = lowerBound; i <= upperBound; i++)
+                        assignments.Add(i);
+                    groupAssignments.Add(assignments.ToArray());
+                    assignments.Clear();
+                }
+                if (groupAssignments[0].Intersect(groupAssignments[1]).ToArray().SequenceEqual(groupAssignments[1])
+                    || groupAssignments[0].Intersect(groupAssignments[1]).ToArray().SequenceEqual(groupAssignments[0]))
+                    count++;
+                groupAssignments.Clear();
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
